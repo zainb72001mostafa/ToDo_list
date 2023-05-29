5ae02d37b5072f2400ea082cf188bb523d661518
zainb72001mostafa <zainb72001mostafa@gmail.com>
2023-05-29 03:43:08 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const listRoute = require('./routes/listsRoutes');
 require('dotenv').config();
 
 const app =express();
 
+app.set('view engine','ejs');
+app.set('views','views');//views folder where we find this template
+app.use(express.static('public'));
 app.use(bodyParser.json());
+
+
 
 var DB_URL=process.env.DB_URL;
 mongoose.connect(DB_URL).then(result=>{
---
+++ routes/listsRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const listController = require('../controllers/listControl');
+
+//router.get('/lists',listController.getAllLists);
+
+module.exports =router;
